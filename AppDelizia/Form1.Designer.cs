2bd3271caf8c897dc545a4b01b3787e3a2c1186e
Zetth717 <cristianr151293@gmail.com>
2024-09-14 18:46:11 -0300

    Update2

--- AppDelizia/Form1.Designer.cs
+++ AppDelizia/Form1.Designer.cs
@@ -36,36 +36,46 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 134);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(510, 248);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(87, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Iniciar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(349, 163);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(603, 248);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(87, 35);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Salir";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(349, 82);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(487, 136);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(228, 38);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(349, 108);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(487, 180);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(228, 38);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
             // 
             // Form1
             // 
--- AppDelizia/Form1.cs
+++ AppDelizia/Form1.cs
@@ -15,11 +15,80 @@ namespace AppDelizia
         public Form1()
         {
             InitializeComponent();
+            SetPlaceholders(); // Establece placeholder al cargar el formulario
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Establece los placeholders para ambos TextBox al cargar el formulario
+        private void SetPlaceholders()
+        {
+            // Placeholder para textBox1
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Usuario";
+                textBox1.ForeColor = Color.Gray;
+            }
+
+            // Placeholder para textBox2
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Contraseña";
+                textBox2.ForeColor = Color.Gray;
+            }
+        }
+
+        // Evento cuando el TextBox1 recibe el foco
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Usuario")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        // Evento cuando el TextBox1 pierde el foco
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Usuario";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+
+        // Evento cuando el TextBox2 recibe el foco
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Contraseña")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = Color.Black;
+            }
+        }
+
+        // Evento cuando el TextBox2 pierde el foco
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Contraseña";
+                textBox2.ForeColor = Color.Gray;
+            }
         }
     }
 }
